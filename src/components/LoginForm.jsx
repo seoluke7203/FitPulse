38c2d5fe3808ff8f60434b258daef31c1bce1ca3
seoluke7203 <seoluke7203@hotmail.com>
2023-07-18 23:27:20 -0700

    added netlify

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -46,8 +46,8 @@ function LoginForm() {
 
 
 
-                            <form name="login" style={{ width: "23rem" }} onSubmit={handleSubmit} method="POST" netlify>
-                            <input type="hidden" name="form-name" value="Contact" />
+                            <form name="login" style={{ width: "23rem" }} onSubmit={handleSubmit} method="POST"data-netlify="true">
+                            <input type="hidden" name="form-name" value="login" />
                                 <h3 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Log in</h3>
 
                                 <div className="form-outline mb-2">
